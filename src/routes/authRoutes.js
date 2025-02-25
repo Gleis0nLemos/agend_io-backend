7c79579c1081697dfad7445eeae9ef0213dfef70
Gleison Lemos <gleison04lemos@gmail.com>
2025-02-25 19:46:03 -0300

    feat: middleware for user authentication

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const { register, login, validateToken} = require('../controllers/authController');
 const authMiddleware = require('../middlewares/authMiddleware');
+const validateUser = require('../middlewares/validateUser');
 
 const router = express.Router();
 
-router.post('/register', register);
+router.post('/register', validateUser, register);
 router.post('/login', login);
 router.get('/validate', authMiddleware, validateToken); // Protected route
 
--- src/server.js
+++ src/server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const cors = require('cors');
 
 require('dotenv').config();
-//console.log('JWT_SECRET:', process.env.JWT_SECRET);
 
 const connectDB = require('./config/db');
 
